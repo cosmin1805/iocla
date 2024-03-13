903dd307d45b9e722ea320c52f3595d1b4d7e5b9
cosmin1805 <cosmin.iacobai@gmail.com>
2024-03-13 17:12:26 +0200

    LAB1-INTELES

--- laborator/content/reprezentare-numere/4-rotations/rotations.c
+++ laborator/content/reprezentare-numere/4-rotations/rotations.c
@@ -1,24 +1,53 @@
 #include <stdio.h>
 
+void printBits(unsigned int num) {
+    // Size of unsigned int in bits
+    int size = sizeof(unsigned int) * 8;
+    
+    // Iterate over each bit from left to right
+    for (int i = size - 1; i >= 0; i--) {
+        // Bitwise shift to check each bit
+        int bit = (num >> i) & 1;
+        // Print the bit
+        printf("%d", bit);
+        
+        // Add space for better readability
+        if (i % 4 == 0)
+            printf(" ");
+    }
+    printf("\n");
+}
 void rotate_left(unsigned int *number, int bits)
 {
-	unsigned int bitMask = -1;
-    
-	(void) number;
-	(void) bits;
+	unsigned int bit_mask = -1;
+    printBits(bit_mask);
+	bit_mask <<= (sizeof(*number) * 8 - bits);
+    printBits(bit_mask);
+	bit_mask &= (*number);
+    printBits(bit_mask);
+	bit_mask >>= (sizeof(*number) * 8 - bits);
+    printBits(bit_mask);
+    printf("\n");
+    printBits(*number);
+	(*number) <<= bits;
+    printBits(*number);
+	(*number) |= bit_mask;
+    printBits(*number);
 }
 
 void rotate_right(unsigned int *number, int bits)
 {
-	/* TODO */
-	(void) number;
-	(void) bits;
+    /* TODO */
+    (void)number;
+    (void)bits;
 }
 
 int main(void)
 {
-	/* TODO: Test functions */
-
-	return 0;
+    /* TODO: Test functions */
+    unsigned int number = 604;
+    rotate_left(&number, 30);
+    printf("%u\n",number);
+    return 0;
 }
 
--- laborator/content/reprezentare-numere/solution/4-rotations/rotations.c
+++ laborator/content/reprezentare-numere/solution/4-rotations/rotations.c
@@ -5,7 +5,6 @@
 void rotate_left(unsigned int *number, int bits)
 {
 	unsigned int bit_mask = -1;
-
 	bit_mask <<= (sizeof(*number) * 8 - bits);
 	bit_mask &= (*number);
 	bit_mask >>= (sizeof(*number) * 8 - bits);
